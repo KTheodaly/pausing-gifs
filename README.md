ff3b89651d5df056b9c837c18bbd1cf6fd5deefb
Theo Daly <theo.daly@outlook.com>
2018-10-13 09:42:53 -0500

    read me and housekeeping

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # pausing-gifs
+## What is this repo project?
+
+* This a gif API
+
+## How does it work?
+
+* Click on the buttons to pull up gifs related to the topics.
+
+* Input your own topic to search. 
+
+  * This site is powered through giphy.
+
+## Who will use this repo?
+
+* This is for coursework utilizing APIs, but is open to anyone who wants to explore the code or play with it for fun.
+
+## What is the goal of the project?
+
+* create a player that pulls gifs from Giphy using API and creates buttons so users can input their own topics.
+* Ten search results with ratings displayed.
--- app.js
+++ app.js
@@ -1,82 +1,81 @@
-//create an array of strings, each one related to a topic that interests you. Save it to a variable called `topics`.
-var topicsVar = {
-    topics: ["floof", "YAS", "Rude", "Spooky", "Ugly", "help", "pupper", "boogie", "no", "omg"],
-    input = ($("input")),
-    newTopic = $input.val().toLowerCase(),
+//i actually think an array is fine without a whole function quite yet here, we'll see
+var topics = ["floof", "YAS", "Rude", "Spooky", "Ugly", "help", "pupper", "boogie", "no", "omg"];
 
+$(document).ready(function () {
+    renderButton();
+    function renderButton() {
+        $("#allbuttons").empty();
+        //here's my for loop to create the buttons
+        for (var i = 0; i < topics.length; i++) {
 
-    //Your app should take the topics in this array and create buttons in your HTML.
-    // * Try using a loop that appends a button for each string in the array.
-
-    addGif: function () {
-        for (i = 0; i < topics.length; i++) {
-            var gifBtn = $("<button>");
-            gifBtn.val(topics[i]);
-            gifBtn.text(topics[i]);
-            gifBtn.attr("onclick", "topicsVar.addGif(this)");
-            topics.append(gifBtn);
+            var newButton = $("<button>");
+            newButton.addClass("itembutton");
+            newButton.addClass("btn btn-success");
+            newButton.text(topics[i]);
+            newButton.attr("data-name", topics[i]);
+            $("#allbuttons").append(newButton);
+            //this should append it to my all buttons div?
         }
-        var topics = $("#topics");
-        $topics.empty();
-        if (newTopic == ! "") {
-            this.topics.push(newTopic);
-        }
-        $input.val("");
-
-
-        //When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
-
-        //$("#button").on("click", function () {
-
-        // Storing our giphy API URL 
-        // var queryURL = "https://api.giphy.com/v1/gifs/search?" + "q=" + topic + "&apikey=qjFxu5atMh8ac6HoyhWZX6FyzvgyMnTD" + "&limit=10";
-
-        // Perfoming an AJAX GET request to our queryURL
-        // $.ajax({
-        //  url: queryURL,
-        //  method: "GET"
-        //  })
-
-        // After the data from the AJAX request comes back
-        //   .then(function (response) {
-
-        // Saving the image_original_url property
-        //    var imageUrl = response.data.image_original_url;
-
-        // Creating and storing an image tag
-        //    var catImage = $("<img>");
-
-        // Setting the catImage src attribute to imageUrl
-        //    catImage.attr("src", imageUrl);
-        //    catImage.attr("alt", "cat image");
-
-        // Prepending the catImage to the images div
-        //    $("#images").prepend(catImage);
-
-
-
-        //When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
-
-        //    gifMove: function (gifClicked) {
-        //create a variable named dataType that is equal to value data-type
-        //        var dataType = gifClicked.getAttribute("data-type");
-        //if the variable a just made equal to that value is exactly equal to still, then set the src attribute to data-animate (from pausing gifs activity in class)
-        //        if (dataType === "still") {
-        //       $(this).attr("src", $(this).attr("data-animate"));
-        //       $(this).attr("data-state", "animate");
-        //        } else {
-        //       $(this).attr("src", $(this).attr("data-still"));
-        //       $(this).attr("data-state", "still");
     }
-}
-                });
+    //trying to create buttons for the user input
+    $("#addbutton").on("click", function (event) {
+
+        event.preventDefault();
+        var addedData = $("#userinput").val().trim();
+        if (addedData != "") {
+            topics.push(addedData);
+            renderButton();
+            $("#userinput").val(" ");
+        }
+
+    });
+    //use the item button class because they all have that class
+    $(document).on("click", ".itembutton", displayInfo);
+
+    function displayInfo() {
+        var itemName = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=food+" + itemName + "&rating=g&limit=10&api_key=qjFxu5atMh8ac6HoyhWZX6FyzvgyMnTD";
+        $("#mainimages").empty();
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+
+                var dataImage = $("<img>");
+                dataImage.attr("src", results[i].images.fixed_height_still.url);
+                dataImage.attr("data-still", results[i].images.fixed_height_still.url);
+                dataImage.attr("data-animate", results[i].images.fixed_height.url);
+                dataImage.addClass("gif");
+                dataImage.attr("data-state", "still");
+
+
+                //i know that displaying the rating looks like this but not sure what to do with it?
+                var gifRating = results[i].rating;
+                var divRating = $("<p>").text("Rating: " + gifRating);
+
+                //should i append this somewhere?
+
+                //$("#mainimages").append
+            }
         });
     }
-
-//Under every gif, display its rating (PG, G, so on).
-//* This data is provided by the GIPHY API.
-//* Only once you get images displaying with button presses should you move on to the next step.
-
-//6. Add a form to your page takes the value from a user input box and adds it into your `topics` array. Then make a function call that takes each topic in the array remakes the buttons on the page.
-
-//Calls the Giphy API and searches for gifs based on the topic button selected
+    //this is just like the cat image, animal vs still
+    $("#mainimages").on("click", ".gif", function () {
+        //but maybe i cant use the div ID not sure what to target 
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        }
+        else if (state === "animate") {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    });
+})
